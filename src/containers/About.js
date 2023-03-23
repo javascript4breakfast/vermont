cd13bda7792c6316bd1d0e5637f86d7da1663f42
javascript4breakfast <49762980+javascript4breakfast@users.noreply.github.com>
2023-03-23 12:01:54 -0700

    about me page, under construction

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BsConeStriped } from 'react-icons/bs';
 
 export default function About() {
     return (
-        <div>About Page</div>
+        <div className='under-construction'>
+            <div className='under-construction-item'>
+                <h1>
+                    <BsConeStriped className='cone' />
+                </h1>
+            </div>
+            <div className='under-construction-item'>
+                <h1>Under Construction</h1>
+            </div>
+            <div className='under-construction-item'>
+                <h2>Please check back later...</h2>
+            </div>
+        </div>
     )
 }
